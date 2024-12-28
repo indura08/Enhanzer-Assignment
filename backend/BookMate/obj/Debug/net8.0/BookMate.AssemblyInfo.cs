3d8cac5443b9da1c93d814dfece560c3db1dd949
indura08 <induraperera3@gmail.com>
2024-12-29 02:04:00 +0530

    minor changes

--- backend/BookMate/obj/Debug/net8.0/BookMate.AssemblyInfo.cs
+++ backend/BookMate/obj/Debug/net8.0/BookMate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5725c897f515d5692c8be639c94acaac266c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
